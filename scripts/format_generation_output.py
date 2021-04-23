3e72592516d163d4b20d322c909d5c1fcccc4e87
john <ioannis.tsiamas@gmail.com>
2021-04-23 02:07:01 +0200

    script for extracting the raw generated text from the output of fairseq-generate

---
+++ scripts/format_generation_output.py
@@ -0,0 +1,44 @@
+import argparse
+from pathlib import Path
+
+
+def format_gen_output(path_to_generation_file: Path) -> None:
+
+    # parse the txt with the generation output from fairseq-generate
+    # generated sentences start with D-$i where $i is the correct order
+    # of the sentence in the dataset
+    raw_generation, correct_order = [], []
+    with open(path_to_generation_file, "r", encoding = "utf8") as f:
+        for line in f.read().splitlines():
+            if line[:2] == "D-":
+                correct_order.append(int(line.split(maxsplit = 1)[0].split("D-")[-1]))
+                raw_generation.append(line.split(maxsplit = 2)[2])
+
+    # fix to correct order
+    raw_generation = [gen for _, gen in sorted(zip(correct_order, raw_generation))]
+
+    # save clean generation txt file
+    clean_generation_path = "_formated.".join(str(path_to_generation_file).rsplit(".", maxsplit = 1))
+    with open(clean_generation_path, "w", encoding = "utf8") as f:
+        for line in raw_generation:
+            f.write(line + "\n")
+
+    print(f"Saved formatted generation at {clean_generation_path}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path_to_generation", "-p", required=True, type=str,
+        help = "Path to a generation file or a directory with generation files.")
+    args = parser.parse_args()
+
+    path_to_generation = Path(args.path_to_generation)
+
+    if path_to_generation.is_file():
+        format_gen_output(path_to_generation)
+    elif path_to_generation.is_dir():
+        for path_to_generation_file in path_to_generation.glob("*.txt"):
+            if "_formated" not in str(path_to_generation):
+                format_gen_output(path_to_generation_file)
+    else:
+        print("Path does not exist")
